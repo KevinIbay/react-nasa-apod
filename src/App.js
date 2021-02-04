4bbe55dd6b2ef702afd8423ef479f2a84bcd8e27
Kevin Ibay <kevinibaycs@gmail.com>
2021-02-03 18:09:20 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { FaRegCopyright } from 'react-icons/fa'
+import config from './config.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [apod, setApod] = useState(null)
+
+  useEffect(() => {
+    getApod()
+  }, [])
+
+  const getApod = async() => {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${config.apiKey}&count=1`)
+    const data = await response.json()
+    setApod(data[0])
+  }
+
+  if (!apod) {
+    return <h1>no image to display</h1>
+  } else {
+    const { copyright, date, explanation, title, url } = apod
+    return (
+  
+      <div className='App'>
+        <div className="container mx-auto p-4 flex flex-col items-center">
+          <h1 className="text-2xl py-2">{title}</h1>
+          <p className="py-3 text-xs md:text-base">{explanation}</p>
+          <button onClick={getApod} className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="button" style={{ transition: "all .15s ease" }}>
+          New Photo
+          </button>
+          <h1 className="text-2xl">{date}</h1>
+          <img src={url}/>
+          {copyright ? `Credit: ${copyright}` : ''}
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
